fb0897f0877718a5930da562bf3710286478610a
baron <baron@bytexgames.ru>
2019-01-31 16:33:01 +0300

    Constructor

--- game.py
+++ game.py
@@ -7,10 +7,20 @@ class Room:
         pass
 
 class Mobile:
+    def __init__(self, name = None):
+        self.name = name
+
     def tick(self):
         pass
+
+    def say(self, phrase):
+        print("{} говорит: {}".format( self.name, phrase))
 
 class Player(Mobile):
     def tick(self):
         pass
 
+vasya = Player('Вася')
+petya = Player("Петя")
+vasya.say("Я вася")
+petya.say("Я петя")
