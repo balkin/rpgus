33a509ba6f7109b5a5cc17cb26f3f2e9626275c1
baron <baron@bytexgames.ru>
2019-01-31 16:38:48 +0300

    Architecture

--- game.py
+++ game.py
@@ -1,10 +1,30 @@
-class Location:
+class World:
+    def __init__(self):
+        self.locations = []
+
     def tick(self):
-        pass
+        for location in self.locations:
+            location.tick()
+
+class Location:
+    def __init__(self):
+        self.rooms = []
+
+    def tick(self):
+        for room in self.rooms:
+            room.tick()
 
 class Room:
+    def __init__(self):
+        self.north = None
+        self.south = None
+        self.west = None
+        self.east = None
+        self.mobiles = []
+
     def tick(self):
-        pass
+        for mobile in self.mobiles:
+            mobile.tick()
 
 class Mobile:
     def __init__(self, name = None):
