d22ba131e0876e66d2e17f91219b266eb95f642b
baron <baron@bytexgames.ru>
2019-01-31 17:22:12 +0300

    Hodilka

--- game.py
+++ game.py
@@ -3,6 +3,7 @@ class World:
         self.locations = []
 
     def tick(self):
+        print("TICK!!!")
         for location in self.locations:
             location.tick()
 
@@ -11,19 +12,41 @@ class Location:
     def __init__(self):
         self.rooms = []
 
+    def generate_rooms(self):
+        pass
+
     def tick(self):
+        if len(self.rooms) == 0:
+            self.generate_rooms()
+
         for room in self.rooms:
             room.tick()
 
 
+class GusGrad(Location):
+
+    def generate_rooms(self):
+        super().generate_rooms()
+        center = Room('Гусьград')
+        left = Room('Левая комната')
+        right = Room("Не левая комната")
+        les = Room("Лес")
+        ozero = Room("Озеро")
+        center.link_west(left)
+        center.link_east(right)
+        left.link_north(les)
+        left.link_south(ozero)
+        self.rooms = [center, left, right, les, ozero]
+
+
 class Room:
-    def __init__(self):
+    def __init__(self, name = 'Комната'):
         self.north = None
         self.south = None
         self.west = None
         self.east = None
         self.mobiles = []
-        self.name = 'Комната'
+        self.name = name
 
     def tick(self):
         for mobile in self.mobiles:
@@ -33,6 +56,21 @@ class Room:
         self.mobiles.append(mobile)
         mobile.room = self
 
+    def link_west(self, another):
+        another.east = self
+        self.west = another
+
+    def link_east(self, another):
+        another.west = self
+        self.east = another
+
+    def link_north(self, another):
+        another.south = self
+        self.north = another
+
+    def link_south(self, another):
+        another.north = self
+        self.south = another
 
 class Mobile:
     def __init__(self, name=None):
@@ -54,7 +92,7 @@ class Mobile:
         self.room = self.room.north
         old.mobiles.remove(self)
         self.room.mobiles.append(self)
-        print("{} двигается на север в комнату {}", self.name, self.room.name)
+        print("{} двигается на север в комнату {}".format(self.name, self.room.name))
 
     def west(self):
         if self.room.west is None:
@@ -65,7 +103,7 @@ class Mobile:
         self.room = self.room.west
         old.mobiles.remove(self)
         self.room.mobiles.append(self)
-        print("{} двигается на запад в комнату {}", self.name, self.room.name)
+        print("{} двигается на запад в комнату {}".format(self.name, self.room.name) )
 
     def east(self):
         if self.room.east is None:
@@ -76,7 +114,7 @@ class Mobile:
         self.room = self.room.east
         old.mobiles.remove(self)
         self.room.mobiles.append(self)
-        print("{} двигается на восток в комнату {}", self.name, self.room.name)
+        print("{} двигается на восток в комнату {}".format(self.name, self.room.name))
 
     def south(self):
         if self.room.south is None:
@@ -87,7 +125,7 @@ class Mobile:
         self.room = self.room.south
         old.mobiles.remove(self)
         self.room.mobiles.append(self)
-        print("{} двигается на юг в комнату {}", self.name, self.room.name)
+        print("{} двигается на юг в комнату {}".format(self.name, self.room.name))
 
     def random_movement(self):
         import random
@@ -115,11 +153,10 @@ vasya = Player('Вася')
 petya = Player("Петя")
 
 world = World()
-gus_city = Location()
+gus_city = GusGrad()
 world.locations.append(gus_city)
-gus_city_main = Room()
-gus_city.rooms.append(gus_city_main)
-
+gus_city.generate_rooms()
+gus_city_main = gus_city.rooms[0]
 gus_city_main.enter(vasya)
 gus_city_main.enter(petya)
 world.tick()
