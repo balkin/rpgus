e8522c05d4cf21917c39d66fa4e36e1e5e941f68
baron <baron@bytexgames.ru>
2019-01-31 16:24:53 +0300

    Initial

---
+++ game.py
@@ -0,0 +1,16 @@
+class Location:
+    def tick(self):
+        pass
+
+class Room:
+    def tick(self):
+        pass
+
+class Mobile:
+    def tick(self):
+        pass
+
+class Player(Mobile):
+    def tick(self):
+        pass
+
